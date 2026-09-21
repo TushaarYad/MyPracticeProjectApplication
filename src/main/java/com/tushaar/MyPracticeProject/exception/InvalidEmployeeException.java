0f6e83a30d6e2565e88e8367527587835296c289
TushaarYad <tushaargopaul29@gmail.com>
2026-09-21 23:55:08 +0400

    Revert "First Commit"
    This reverts commit 0358e68e3a3ea76e8eb0544cdda454c7db6d69a2.

--- src/main/java/com/tushaar/MyPracticeProject/exception/InvalidEmployeeException.java
+++
@@ -1,8 +0,0 @@
-package com.tushaar.MyPracticeProject.exception;
-
-//Here used for post requests if ever data entered by client was invalid
-public class InvalidEmployeeException extends RuntimeException{
-    public InvalidEmployeeException(String message) {
-        super(message);
-    }
-}
